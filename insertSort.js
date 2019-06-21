30377bbb33227b7dfc78a365227582e93fac1320
SambaLim <limsungho07@hanmail.net>
2019-06-22 00:21:46 +0900

    [Upd] insertionSort

--- insertSort.js
+++ insertSort.js
@@ -3,8 +3,8 @@
  */
 
 var insertionSort = (array) => {
-    let i = 1;
-    let j, temp;
+    var i = 1;
+    var j, temp;
 
     // array가 비었는지 확인
     arrayIsEmpty(array);
@@ -13,13 +13,16 @@ var insertionSort = (array) => {
         // 비교를 할 숫자를 선택
         temp = array[i];
 
-        for(j = i-1; j=>0; j--) {
-
-            if(array[j] > temp) {
-                
-            }
+        for(j = i-1; j>=0 && temp < array[j]; j--) {
+            // 비교를 할 숫자와 그 앞의 수들과 비교
+            array[j+1] = array[j];
         }
+
+        array[j+1] = temp;
     }
+    console.log(array);
+
+    return array;
 }
 
 var arrayIsEmpty = (array) => {
