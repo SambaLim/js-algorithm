9f673b3ec7277a97f97d8d44ee57e9b152e22470
SambaLim <limsungho07@hanmail.net>
2019-06-23 23:29:23 +0900

    [Upd] mergeSort

--- mergeSort.js
+++ mergeSort.js
@@ -11,9 +11,33 @@ var mergeSort = (array) => {
     let pivot = array.length/2;
     let leftSide = array.slice(0, pivot);
     let rightSide = array.slice(pivot, array.length);
+
+    return merge(mergeSort(leftSide), mergeSort(rightSide));
 }
 
 var merge = (leftSide, rightSide) => {
     let result = [];
     
+    while(leftSide.length == rightSide.length) {
+
+        // 비교 후, 작은 수 부터 결과에 넣어줌
+        if(leftSide[0] <= rightSide[0]) {
+            result.push(leftSide.shift());
+        } else {
+            result.push(rightSide.shift());
+        }
+
+        // 남은 애들 넣기
+        while(leftSide.length) {
+            result.push(leftSide.shift());
+        }
+
+        while(rightSide.length) {
+            result.push(rightSide.shift());
+        }
+
+        console.log(result);
+
+        return result;
+    }
 }
