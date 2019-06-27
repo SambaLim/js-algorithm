962ae5fd25f2babe5abfb4208a31be589df02e4d
SambaLim <limsungho07@hanmail.net>
2019-06-27 23:29:07 +0900

    [upd] radixSort

--- sort/radixSort.js
+++ sort/radixSort.js
@@ -3,8 +3,18 @@
  */
 var radixSort = (array) => {
 
+    // LSD
+
     if(isEmptyOROne(array)) {
         return array;
     }
-    
+
+    // 자리수를 판별하기 위함
+    let mod = 10;
+    let i;
+    let digit = 0;
+
+    for(i=0; i < array.lengh; i++) {
+        
+    }
 }
