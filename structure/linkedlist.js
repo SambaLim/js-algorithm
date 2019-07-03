b5202b2348326af33e9a738bf851291ed652819c
SambaLim <limsungho07@hanmail.net>
2019-07-04 00:09:17 +0900

    [upd] mLinkedList wrong

--- structure/linkedlist.js
+++ structure/linkedlist.js
@@ -1,15 +1,24 @@
 var mLinkedList = (function(mLinkedList) {
 
-    let length = 0;
-    let head = null;
+    let _length = 0;
+    let _head = null;
 
     function Node(data) {
         this.data = data;
         this.next = null;
     }
 
+    // 외부에서 실행될 함수
+    mLinkedList.length = _length;
+    
     mLinkedList.add = function(value) {
         let node = new Node(value);
+
+        if(!_head) {
+            _head = node;
+            _length++;
+            return node;
+        }
     }
 
     return mLinkedList;
