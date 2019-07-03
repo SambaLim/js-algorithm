d1a0c3f7f6cc5fba0539a6bccbc134727611ded0
SambaLim <limsungho07@hanmail.net>
2019-07-03 23:44:39 +0900

    [upd] linkedlist

--- structure/linkedlist.js
+++ structure/linkedlist.js
@@ -1,3 +1,14 @@
-var LinkedList = (function() {
+var mLinkedList = (function(mLinkedList) {
 
-})();
+    let length = 0;
+    let head = null;
+
+    function Node(data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    console.log(mLinkedList.prototype);
+
+
+})(window.mLinkedList);
