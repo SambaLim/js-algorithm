b4af984e7de67500fd2fac1621a042640fd2ec85
SambaLim <limsungho07@hanmail.net>
2019-07-14 22:06:11 +0900

    [Upd] radixSort

--- sort/radixSort.js
+++ sort/radixSort.js
@@ -10,12 +10,15 @@ var radixSort = (array, digit) => {
 
     // 자리수를 판별하기 위함
     let mod = 10;
-    let i;
+    let i, j;
     if(!digit) digit = 10;
 
     // 자리수가 몇인지 알아보기
     for(i=0; i < digit; mod *= 10) {
-        
+        for(j=0; j < array.length; j++) {
+            // 같은 그룹을 묶기
+            var bucket = parseInt(array[j] % mod);
+        }
     }
 
     // 같은 자리수 친구들끼리 한 배열에
